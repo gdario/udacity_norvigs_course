392ffb9097a86ce6f1e540dfc5a2657835d9b148
Giovanni d'Ario <giovanni.dario@gmail.com>
2024-01-02 16:48:09 +0100

    Start lesson3. Restart at 'Compiling'.

--- src/subpalindrome.py
+++ src/subpalindrome.py
@@ -15,12 +15,47 @@
 #
 # Please do not use regular expressions to solve this quiz!
 
+
 def longest_subpalindrome_slice(text):
     "Return (i, j) such that text[i:j] is the longest palindrome in text."
     # Your code here
-    
+    if text == '':
+        return (0, 0)
+    def length(slice):
+        a, b = slice
+        return b - a
+    candidates = [grow(text, start, end)
+                  for start in range(len(text))
+                  for end in (start, start + 1)]
+    return max(candidates, key=length)
+
+
+def grow(text, start, end):
+    "Start with a 0- or 1- length palindrome. Try to grow a bigger one."
+    while (start > 0 and end < len(text)
+           and text[start-1].upper() == text[end].upper()):
+        start -= 1
+        end += 1
+    return (start, end)
+
+
+def my_longest_subpalindrome_slice(text):
+    "Return (i, j) such that text[i:j] is the longest palindrome in text."
+    text = text.lower()
+    nchar = len(text)
+    for k in range(nchar):
+        for i in range(0, k + 1):
+            j = nchar - k + i
+            tmp = text[i:j]
+            if tmp == tmp[::-1]:
+                return (i, j)
+    return (0, 0)
+
+
+
 def test():
-    L = longest_subpalindrome_slice
+    # L = longest_subpalindrome_slice
+    L = my_longest_subpalindrome_slice
     assert L('racecar') == (0, 7)
     assert L('Racecar') == (0, 7)
     assert L('RacecarX') == (0, 7)
